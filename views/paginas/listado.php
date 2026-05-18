70d33b725211b3136ae8ad6000e2cf08a0a8b908
Diego <dapd2004@gmail.com>
2026-05-18 09:44:03 -0400

    Actualizando las rutas de imagenes

--- views/paginas/listado.php
+++ views/paginas/listado.php
@@ -2,7 +2,7 @@
     <?php foreach($propiedades as $propiedad): ?>
     <div class="anuncio">
         
-        <img class="img2" src="/public/imagenes/<?php echo $propiedad->imagenes; ?>" alt="anuncio" loading="lazy">
+        <img class="img2" src="imagenes/<?php echo $propiedad->imagenes; ?>" alt="anuncio" loading="lazy">
 
                 <div class="contenido-anuncio">
                     <h3><?php echo $propiedad->titulo; ?></h3>
@@ -10,19 +10,19 @@
                     <p class="precio">$<?php echo $propiedad->precio; ?></p>
                     <ul class="iconos-caracteristicas">
                         <li>
-                            <img class="icono" src="/public/build/img/icono_wc.svg" alt="icono baños">
+                            <img class="icono" src="build/img/icono_wc.svg" alt="icono baños">
                             <p><?php echo $propiedad->WC; ?></p>
                         </li>
                         <li>
-                            <img class="icono" src="/public/build/img/icono_dormitorio.svg" alt="icono dormitorio">
+                            <img class="icono" src="build/img/icono_dormitorio.svg" alt="icono dormitorio">
                             <p><?php echo $propiedad->habitaciones; ?></p>
                         </li>
                         <li>
-                            <img class="icono" src="/public/build/img/icono_estacionamiento.svg" alt="icono baños">
+                            <img class="icono" src="build/img/icono_estacionamiento.svg" alt="icono baños">
                             <p><?php echo $propiedad->estacionamiento; ?></p>
                         </li>
                     </ul>
-                    <a href="/public/propiedad?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">
+                    <a href="propiedad?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">
                         ver propiedad
                     </a>
         </div> <!-- contenido del anuncio -->
