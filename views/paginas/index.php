c1eb5923cf02ceb11295d8426f099783114ab9af
Diego <dapd2004@gmail.com>
2026-05-18 09:49:26 -0400

    Actualizando las rutas del html e imagenes

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -2,17 +2,17 @@
         <h1>Más sobre nosotros</h1>
         <div class="iconos-nosotros">
             <div class="icono">
-                <img src="/public/build/img/icono1.svg" alt="Icono Seguridad" loading="lazy">
+                <img src="build/img/icono1.svg" alt="Icono Seguridad" loading="lazy">
                 <h3>Seguridad</h3>
                 <p>Cum excepturi id nobis aliquam delectus repellat nemo doloremque corrupti consequuntur eos? Temporibus aliquam molestias veritatis porro nostrum ut hic minima amet?</p>
             </div>
             <div class="icono">
-                <img src="/public/build/img/icono2.svg" alt="Icono Precio" loading="lazy">
+                <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
                 <h3>Precio</h3>
                 <p>Cum excepturi id nobis aliquam delectus repellat nemo doloremque corrupti consequuntur eos? Temporibus aliquam molestias veritatis porro nostrum ut hic minima amet?</p>
             </div>
             <div class="icono">
-                <img src="/public/build/img/icono3.svg" alt="Icono tiempo" loading="lazy">
+                <img src="build/img/icono3.svg" alt="Icono tiempo" loading="lazy">
                 <h3>A tiempo</h3>
                 <p>Cum excepturi id nobis aliquam delectus repellat nemo doloremque corrupti consequuntur eos? Temporibus aliquam molestias veritatis porro nostrum ut hic minima amet?</p>
             </div>
@@ -25,14 +25,14 @@
         <?php $limite = 3; include 'listado.php'; ?>
 
         <div class="alinear-derecha">
-            <a href="/public/propiedades" class="boton-verde">Ver todas</a>
+            <a href="/propiedades" class="boton-verde">Ver todas</a>
         </div>
     </section>
 
     <section class="imagen-contacto">
         <h2>Encuentra la casa de tus sueños</h2>
         <p>Llena el formulario a continuacion y un asesor se pondra en contacto contingo en la brevedad</p>
-        <a href="/public/contacto" class="boton-amarillo-block">Contactanos</a>
+        <a href="/contacto" class="boton-amarillo-block">Contactanos</a>
     </section>
 
     <div class="contenedor seccion seccion-inferior">
@@ -42,13 +42,13 @@
             <article class="entrada-blog">
                 <div class="image">
                     <picture>
-                        <source srcset="/public/build/img/blog1.webp" type="img/webp" >
-                        <source srcset="/public/build/img/blog1.jpg" type="img/jpeg" >
-                        <img src="/public/build/img/blog1.jpg" alt="imagen de entrada de blog" loading="lazy">
+                        <source srcset="build/img/blog1.webp" type="img/webp" >
+                        <source srcset="build/img/blog1.jpg" type="img/jpeg" >
+                        <img src="build/img/blog1.jpg" alt="imagen de entrada de blog" loading="lazy">
                     </picture>
                 </div>
                 <div class="texto-entrada">
-                    <a href="/public/entrada">
+                    <a href="/entrada">
                         <h4>Terraza en el techo de tu casa</h4>
                         <p class="informacion-meta">Escrito el: <span>20/10/2025</span> por: <span>Admin</span></p>
 
@@ -59,13 +59,13 @@
             <article class="entrada-blog">
                 <div class="image">
                     <picture>
-                        <source srcset="/public/build/img/blog2.webp" type="img/webp" >
-                        <source srcset="/public/build/img/blog2.jpg" type="img/jpeg" >
-                        <img src="/public/build/img/blog2.jpg" alt="imagen de entrada de blog" loading="lazy">
+                        <source srcset="build/img/blog2.webp" type="img/webp" >
+                        <source srcset="build/img/blog2.jpg" type="img/jpeg" >
+                        <img src="build/img/blog2.jpg" alt="imagen de entrada de blog" loading="lazy">
                     </picture>
                 </div>
                 <div class="texto-entrada">
-                    <a href="/public/entrada">
+                    <a href="/entrada">
                         <h4>Guía para la decoración de tu hogar</h4>
                         <p class="informacion-meta">Escrito el: <span>20/10/2025</span> por: <span>Admin</span></p>
 
